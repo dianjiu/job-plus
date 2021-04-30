0bebb2829641a4608dc10b753c965deb53626393
mengwei <mengwei@dianjiu.cc>
2021-04-30 15:36:22 +0800

    修改数据库名称，默认本地启动

--- src/main/java/cn/org/dianjiu/task/Application.java
+++ src/main/java/cn/org/dianjiu/task/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.setProperty("cfg.env","test");
+        System.setProperty("cfg.env","local");
         SpringApplication.run(Application.class, args);
     }
 
